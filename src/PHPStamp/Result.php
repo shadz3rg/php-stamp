02dca3806e69ce7c7452fbda2c0150e2b36a95cf
atari-pc <ukhardc0r3@gmail.com>
2018-04-07 23:27:58 +0300

    Result class refactor, added save and output methods

--- src/PHPStamp/Result.php
+++ src/PHPStamp/Result.php
@@ -53,21 +53,15 @@ class Result
             $fileName = $this->document->getDocumentName();
         }
 
-        $tempArchive = tempnam(sys_get_temp_dir(), 'doc');
-
-        if (copy($this->document->getDocumentPath(), $tempArchive) === true) {
-            $zip = new \ZipArchive();
-            $zip->open($tempArchive);
-            $zip->addFromString($this->document->getContentPath(), $this->output->saveXML());
-            $zip->close();
-
+        $tempFile = $this->buildFile();
+        if ($tempFile !== false) {
             header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
             header('Content-Disposition: attachment;filename="' . $fileName . '"');
 
             // Send file - required ob_clean() & exit;
             if (ob_get_contents()) ob_clean();
-            readfile($tempArchive);
-            unlink($tempArchive);
+            readfile($tempFile);
+            unlink($tempFile);
             exit;
         }
     }
@@ -75,7 +69,7 @@ class Result
     /**
      * Merge XML result with original document into temp file.
      *
-     * @return false|string Path to built file.
+     * @return false|string Path to built file or false on some error.
      */
     public function buildFile()
     {
@@ -86,6 +80,47 @@ class Result
             $zip->addFromString($this->document->getContentPath(), $this->output->saveXML());
             $zip->close();
             return $tempArchive;
+        }
+
+        return false;
+    }
+
+    /**
+     * Build file and save to filesystem.
+     *
+     * @param string $destinationPath Destination dir with no trailing slash.
+     * @param string|null $fileName File name, use original document name if no value present.
+     * @return bool
+     */
+    public function save($destinationPath, $fileName = null)
+    {
+        if ($fileName === null) {
+            $fileName = $this->document->getDocumentName();
+        }
+
+        $tempFile = $this->buildFile();
+        if ($tempFile !== false) {
+            $result = copy($tempFile, $destinationPath . '/' . $fileName);
+            unlink($tempFile);
+            return $result;
+        }
+
+        return false;
+    }
+
+    /**
+     * Build file and output to buffer.
+     * Useful for framework integration, such as Symfony Response object.
+     *
+     * @return string|false File content or false on error.
+     */
+    public function output()
+    {
+        $tempFile = $this->buildFile();
+        if ($tempFile !== false) {
+            $output = file_get_contents($tempFile);
+            unlink($tempFile);
+            return $output;
         }
 
         return false;
