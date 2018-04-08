7d901d02d33cb185e6c7fc23dc83433dd8665295
atari-pc <ukhardc0r3@gmail.com>
2018-04-08 12:42:24 +0300

    Tag documentation

--- src/PHPStamp/Processor/Tag.php
+++ src/PHPStamp/Processor/Tag.php
@@ -1,66 +1,111 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shooz
- * Date: 21.09.14
- * Time: 14:52
- */
 
 namespace PHPStamp\Processor;
-
 
 class Tag
 {
     /**
+     * Tag data.
+     *
      * @var array
      */
     private $summary;
+
     /**
+     * Path to value in placeholder.
+     *
      * @var array
      */
     private $path;
+
     /**
+     * Parsed functions.
+     *
      * @var array
      */
     private $functions;
 
+    /**
+     * Creates a new Tag.
+     *
+     * @param array $summary Tag data.
+     * @param array $path Path to value in placeholder.
+     * @param array $functions Parsed functions.
+     */
     public function __construct(array $summary, array $path, array $functions)
     {
         $this->summary = $summary;
         $this->path = $path;
         $this->functions = $functions;
+
     }
 
+    /**
+     * Placeholder position inside node content.
+     * Left padding of string.
+     *
+     * @return int
+     */
     public function getPosition()
     {
         return $this->summary['position'];
     }
 
+    /**
+     * Length of tag with brackets.
+     *
+     * @return int
+     */
     public function getLength()
     {
         return $this->summary['length'];
     }
 
+    /**
+     * XPath presentation of value path.
+     *
+     * @return string
+     */
     public function getXmlPath()
     {
         return implode('/', $this->path);
     }
 
+    /**
+     * Last part of the path.
+     *
+     * @return string
+     */
     public function getRelativePath()
     {
         return end($this->path);
     }
 
+    /**
+     * Has function helper.
+     *
+     * @return bool
+     */
     public function hasFunctions()
     {
         return (count($this->functions) !== 0);
     }
 
+    /**
+     * Get parsed functions.
+     *
+     * @return array
+     */
     public function getFunctions()
     {
         return $this->functions; // TODO improve
     }
 
+    /**
+     * Get placeholder content with brackets.
+     *
+     * @return string
+     */
     public function getTextContent()
     {
         return $this->summary['textContent'];
